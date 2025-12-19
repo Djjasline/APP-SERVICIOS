8d558d602f50de47592fab8d083004e390f2fe90
Djjasline <134792819+Djjasline@users.noreply.github.com>
2025-12-19 13:21:36 -0500

    Update Routes.jsx

--- src/app/inspeccion/Routes.jsx
+++ src/app/inspeccion/Routes.jsx
@@ -1,15 +1,14 @@
+import React from "react";
 import { Routes, Route } from "react-router-dom";
 
-import ReportHistoryManagement from "./pages/report-history-management";
 import HojaInspeccionHidro from "./HojaInspeccionHidro";
 import HojaInspeccionBarredora from "./pages/barredora";
 
 export default function InspeccionRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<ReportHistoryManagement />} />
-      <Route path="/hidro" element={<HidroInspeccion />} />
-      <Route path="/barredora" element={<BarredoraInspeccion />} />
+      <Route path="hidro" element={<HojaInspeccionHidro />} />
+      <Route path="barredora" element={<HojaInspeccionBarredora />} />
     </Routes>
   );
 }
