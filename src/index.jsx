8b0ce4485b2e6b20d58761e475ed98409c1863f4
Djjasline <134792819+Djjasline@users.noreply.github.com>
2025-12-23 14:36:42 -0500

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,47 +1,10 @@
-// src/Routes.jsx
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-import PanelServicios from "./pages/panel-servicios";
-import ReportHistoryManagement from "./pages/report-history-management";
-import ServiceReportCreation from "./pages/service-report-creation";
-import PDFReportPreview from "./pages/pdf-report-preview";
-import NotFound from "./pages/NotFound";
-
-// Inspecciones
-import HojaInspeccionHidro from "./app/inspeccion/Hidro";
-import HojaInspeccionBarredora from "./app/inspeccion/Barredora";
-import HojaInspeccionCamara from "./app/inspeccion/Camara";
-
-export default function AppRoutes() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        {/* MENÚ PRINCIPAL */}
-        <Route path="/" element={<PanelServicios />} />
-
-        {/* INFORMES */}
-        <Route
-          path="/report-history-management"
-          element={<ReportHistoryManagement />}
-        />
-        <Route
-          path="/service-report-creation"
-          element={<ServiceReportCreation />}
-        />
-        <Route path="/pdf-report-preview" element={<PDFReportPreview />} />
-
-        {/* INSPECCIONES */}
-        <Route path="/inspeccion/hidro" element={<HojaInspeccionHidro />} />
-        <Route
-          path="/inspeccion/barredora"
-          element={<HojaInspeccionBarredora />}
-        />
-        <Route path="/inspeccion/camara" element={<HojaInspeccionCamara />} />
-
-        {/* 404 */}
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
