03afaaec057420445e85beebc73c9fdbd84131e9
Djjasline <134792819+Djjasline@users.noreply.github.com>
2025-12-23 16:02:01 -0500

    Update App.jsx

--- src/app/inspeccion/App.jsx
+++ src/app/inspeccion/App.jsx
@@ -1,17 +1,5 @@
-import { Routes, Route, Navigate } from "react-router-dom";
-import InspectionList from "./components/InspectionList";
-import HojaInspeccionHidro from "./components/HojaInspeccionHidro";
+import Routes from "./Routes";
 
 export default function App() {
-  return (
-    <div className="min-h-screen bg-slate-50">
-      <main className="py-6">
-        <Routes>
-          <Route path="/" element={<InspectionList />} />
-          <Route path="/hoja/:id" element={<HojaInspeccionHidro />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-      </main>
-    </div>
-  );
+  return <Routes />;
 }
