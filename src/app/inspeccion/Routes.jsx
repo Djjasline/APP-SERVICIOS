e4d2baf9e6dc50cce8e1185215473ede9f8dd279
Djjasline <134792819+Djjasline@users.noreply.github.com>
2025-12-24 12:54:22 -0500

    Update Routes.jsx

--- src/app/inspeccion/Routes.jsx
+++ src/app/inspeccion/Routes.jsx
@@ -1,17 +1,20 @@
-import { Routes, Route } from "react-router-dom";
+// src/Routes.jsx
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import IndexInspeccion from "./pages/IndexInspeccion";
-import HojaInspeccionHidro from "./HojaInspeccionHidro";
-import HojaInspeccionBarredora from "./HojaInspeccionBarredora";
-import HojaInspeccionCamara from "./HojaInspeccionCamara";
+import ScrollToTop from "./components/ScrollToTop";
+import ErrorBoundary from "./components/ErrorBoundary";
 
-export default function InspectionRoutes() {
-  return (
-    <Routes>
-      <Route index element={<IndexInspeccion />} />
-      <Route path="hidro" element={<HojaInspeccionHidro />} />
-      <Route path="barredora" element={<HojaInspeccionBarredora />} />
-      <Route path="camara" element={<HojaInspeccionCamara />} />
-    </Routes>
-  );
-}
+// Páginas principales
+import PanelServicios from "./pages/PanelServicios";
+import NotFound from "./pages/NotFound";
+
+// Módulo informes de servicio
+import ServiceReportCreation from "./pages/service-report-creation";
+import PDFReportPreview from "./pages/pdf-report-preview";
+import ReportHistoryManagement from "./pages/report-history-management";
+import EmailIntegrationInterface from "./pages/email-integration-interface";
+import DigitalSignatureCapture from "./pages/digital-signature-capture";
+
+// Módulo inspección (rutas hijas)
+import InspectionRoutes from "./app/ins
