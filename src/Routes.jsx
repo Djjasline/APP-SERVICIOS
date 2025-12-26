1684ab40cd4860a9f6327d348af4c55bc87fa76c
Djjasline <134792819+Djjasline@users.noreply.github.com>
2025-12-26 18:00:01 -0500

    Refactor routing structure in Routes.jsx

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,36 +1,16 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import PanelServicios from "./pages/PanelServicios";
-import ReportHistoryManagement from "./pages/report-history-management";
-import ServiceReportCreation from "./pages/service-report-creation";
-
+import PanelServicios from "./pages/panel-servicios";
 import InspectionRoutes from "./app/inspeccion/Routes";
-import NotFound from "./pages/NotFound";
 
-export default function AppRoutes() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        {/* MENÚ PRINCIPAL */}
-        <Route path="/" element={<PanelServicios />} />
+<RouterRoutes>
+  {/* PANEL GENERAL */}
+  <Route path="/" element={<PanelServicios />} />
 
-        {/* INFORMES */}
-        <Route
-          path="/report-history-management"
-          element={<ReportHistoryManagement />}
-        />
-        <Route
-          path="/service-report-creation"
-          element={<ServiceReportCreation />}
-        />
+  {/* INFORMES */}
+  <Route path="/reports" element={<ReportHistoryManagement />} />
+  <Route path="/service-report-creation" element={<ServiceReportCreation />} />
 
-        {/* INSPECCIONES */}
-        <Route path="/inspeccion/*" element={<InspectionRoutes />} />
+  {/* INSPECCIONES */}
+  <Route path="/inspeccion/*" element={<InspectionRoutes />} />
 
-        {/* 404 */}
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
+  <Route path="*" element={<NotFound />} />
+</RouterRoutes>
