2ee5d76182e819da127a5f1078564493cc5ddfa4
Djjasline <134792819+Djjasline@users.noreply.github.com>
2026-01-05 12:53:37 -0500

    Create inspeccionBarredoraSchema.js

---
+++ src/app/inspeccion/schemas/inspeccionBarredoraSchema.js
@@ -0,0 +1,20 @@
+export const preServicio = [
+  { codigo: "1.1", descripcion: "Arranque y pruebas iniciales" },
+  { codigo: "1.2", descripcion: "Funcionamiento general" },
+];
+
+export const sistemaBarrido = [
+  { codigo: "A.1", descripcion: "Cepillos principales" },
+  { codigo: "A.2", descripcion: "Cepillos laterales" },
+  { codigo: "A.3", descripcion: "Sistema de aspiración" },
+];
+
+export const sistemaHidraulico = [
+  { codigo: "B.1", descripcion: "Fugas hidráulicas" },
+  { codigo: "B.2", descripcion: "Nivel de aceite" },
+];
+
+export const sistemaElectrico = [
+  { codigo: "C.1", descripcion: "Tablero de control" },
+  { codigo: "C.2", descripcion: "Iluminación y alarmas" },
+];
