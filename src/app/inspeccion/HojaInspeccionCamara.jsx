b8b80e9d5bd997dd2c92085f3e5b03792b7a06a1
Djjasline <134792819+Djjasline@users.noreply.github.com>
2026-01-05 11:55:51 -0500

    Update HojaInspeccionCamara.jsx

--- src/app/inspeccion/HojaInspeccionCamara.jsx
+++ src/app/inspeccion/HojaInspeccionCamara.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { markInspectionCompleted } from "./utils/inspectionStorage";
+import React from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import { markInspectionCompleted } from "@utils/inspectionStorage";
 import { useNavigate } from "react-router-dom";
 
 export default function HojaInspeccionCamara() {
