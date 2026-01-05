8f9ae790f390e394d5be18bb24aeef8046638130
Djjasline <134792819+Djjasline@users.noreply.github.com>
2026-01-05 12:14:55 -0500

    Create formStates.js

---
+++ src/utils/formStates.js
@@ -0,0 +1,14 @@
+export const FORM_STATES = {
+  BORRADOR: "borrador",
+  COMPLETADO: "completado",
+};
+
+export const FORM_STATE_LABELS = {
+  borrador: "Borrador",
+  completado: "Completado",
+};
+
+export const FORM_STATE_STYLES = {
+  borrador: "bg-yellow-100 text-yellow-800",
+  completado: "bg-green-100 text-green-800",
+};
