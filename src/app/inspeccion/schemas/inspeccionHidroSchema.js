399ef31901c9a87565bb6e472b10b6cd476be93a
Djjasline <134792819+Djjasline@users.noreply.github.com>
2026-01-05 12:48:47 -0500

    Create inspeccionHidroSchema.js

---
+++ src/app/inspeccion/schemas/inspeccionHidroSchema.js
@@ -0,0 +1,25 @@
+export const preServicio = [
+  { codigo: "1.1", descripcion: "Pruebas de encendido del equipo" },
+  { codigo: "1.2", descripcion: "Funcionamiento de sistemas" },
+];
+
+export const sistemaHidraulicoAceite = [
+  { codigo: "A.1", descripcion: "Fugas de aceite hidráulico" },
+  { codigo: "A.2", descripcion: "Nivel de aceite hidráulico" },
+  { codigo: "A.3", descripcion: "Estado de filtros hidráulicos" },
+];
+
+export const sistemaHidraulicoAgua = [
+  { codigo: "B.1", descripcion: "Fugas de agua en mangueras" },
+  { codigo: "B.2", descripcion: "Estado de válvulas" },
+];
+
+export const sistemaElectrico = [
+  { codigo: "C.1", descripcion: "Funcionamiento de tablero" },
+  { codigo: "C.2", descripcion: "Luces y accesorios" },
+];
+
+export const sistemaSuccion = [
+  { codigo: "D.1", descripcion: "Estado de mangueras de succión" },
+  { codigo: "D.2", descripcion: "Funcionamiento de soplador" },
+];
