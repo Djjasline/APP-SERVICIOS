99e4e3efc2cd992510732e7fea46e88359e15db2
Djjasline <134792819+Djjasline@users.noreply.github.com>
2026-01-06 10:30:57 -0500

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Aplicacion from "./Aplicacion";
+import App from "./App";
 import "./index.css";
-
-import { ProveedorDeInformes } from "./contexto/ReportContext";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root")
@@ -11,8 +9,6 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <ProveedorDeInformes>
-      <Aplicacion />
-    </ProveedorDeInformes>
+    <App />
   </React.StrictMode>
 );
