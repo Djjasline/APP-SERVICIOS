86d026702bd7c9e91fd0d24847b565a666521423
Djjasline <134792819+Djjasline@users.noreply.github.com>
2026-01-06 13:32:57 -0500

    Update Routes.jsx

--- src/app/mantenimiento/Routes.jsx
+++ src/app/mantenimiento/Routes.jsx
@@ -1,6 +1,4 @@
-import React from "react";
 import { Routes, Route } from "react-router-dom";
-
 import IndexMantenimiento from "./IndexMantenimiento";
 import HojaMantenimientoHidro from "./HojaMantenimientoHidro";
 import HojaMantenimientoBarredora from "./HojaMantenimientoBarredora";
@@ -8,7 +6,7 @@ import HojaMantenimientoBarredora from "./HojaMantenimientoBarredora";
 export default function MantenimientoRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<IndexMantenimiento />} />
+      <Route index element={<IndexMantenimiento />} />
       <Route path="hidro" element={<HojaMantenimientoHidro />} />
       <Route path="barredora" element={<HojaMantenimientoBarredora />} />
     </Routes>
