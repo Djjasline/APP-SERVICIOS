7b1b2b9e2f82930a8197c1f4ba1e7b12f35dba99
Djjasline <134792819+Djjasline@users.noreply.github.com>
2026-01-07 16:15:08 -0500

    Update IndexInspeccion.jsx

--- src/app/inspeccion/pages/IndexInspeccion.jsx
+++ src/app/inspeccion/pages/IndexInspeccion.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-const id = crypto.randomUUID();
 import { getInspectionHistory } from "@utils/inspectionStorage";
 
 /* =========================
