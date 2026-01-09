7e480400030679d9a55e29b0b19f3def02d09957
Djjasline <134792819+Djjasline@users.noreply.github.com>
2026-01-09 09:21:23 -0500

    Update Routes.jsx

--- src/app/inspeccion/Routes.jsx
+++ src/app/inspeccion/Routes.jsx
@@ -1,39 +1,28 @@
-// APP-SERVICIOS/src/app/inspeccion/Routes.jsx
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import FirmaInspeccion from "./FirmaInspeccion";
-
-<Route path="hidro/:id/firma" 
-element={<FirmaInspeccion />} 
-/>
 
 import IndexInspeccion from "./pages/IndexInspeccion";
 import HojaInspeccionHidro from "./HojaInspeccionHidro";
 import HojaInspeccionBarredora from "./HojaInspeccionBarredora";
 import HojaInspeccionCamara from "./HojaInspeccionCamara";
 import HistorialInspecciones from "./HistorialInspecciones";
-
-/* ✅ NUEVO: pantalla aislada de firma */
-import HojaFirma from "./HojaFirma";
+import HojaFirma from "./HojaFirma"; // 👈 IMPORTANTE
 
 export default function InspectionRoutes() {
   return (
     <Routes>
-      {/* MENÚ DE INSPECCIONES */}
+      {/* MENÚ */}
       <Route index element={<IndexInspeccion />} />
 
-      {/* HISTORIAL HIDRO */}
-      <Route
-        path="hidro/historial"
-        element={<HistorialInspecciones />}
-      />
+      {/* HISTORIAL */}
+      <Route path="hidro/historial" element={<HistorialInspecciones />} />
 
-      {/* FORMULARIOS CON ID */}
+      {/* FORMULARIOS */}
       <Route path="hidro/:id" element={<HojaInspeccionHidro />} />
       <Route path="barredora/:id" element={<HojaInspeccionBarredora />} />
       <Route path="camara/:id" element={<HojaInspeccionCamara />} />
 
-      {/* ✅ NUEVA RUTA DE FIRMA (AISLADA, SEGURA) */}
+      {/* FIRMA (ESTA ERA LA QUE FALTABA) */}
       <Route path="hidro/:id/firma" element={<HojaFirma />} />
     </Routes>
   );
