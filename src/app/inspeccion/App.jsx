d2b5d0c5b164b53764c3196879533e949fdb2edb
Djjasline <134792819+Djjasline@users.noreply.github.com>
2026-01-09 10:00:56 -0500

    Update App.jsx

--- src/app/inspeccion/App.jsx
+++ src/app/inspeccion/App.jsx
@@ -1,10 +1,5 @@
-import { BrowserRouter } from "react-router-dom";
 import Routes from "./Routes";
 
 export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes />
-    </BrowserRouter>
-  );
+  return <Routes />;
 }
