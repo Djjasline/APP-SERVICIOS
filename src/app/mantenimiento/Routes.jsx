879c270959da5c69ea3dbf0a3117a0dd73e8ee57
Djjasline <134792819+Djjasline@users.noreply.github.com>
2026-01-13 16:27:37 -0500

    Update Routes.jsx

--- src/app/mantenimiento/Routes.jsx
+++ src/app/mantenimiento/Routes.jsx
@@ -1,14 +1,41 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
+
 import IndexMantenimiento from "./IndexMantenimiento";
-import HojaMantenimientoHidro from "./HojaMantenimientoHidro";
-import HojaMantenimientoBarredora from "./HojaMantenimientoBarredora";
+
+// HIDRO
+import MantenimientoHidroHome from "./hidro/Home";
+import HojaMantenimientoHidro from "./hidro/Form";
+
+// BARREDORA
+import MantenimientoBarredoraHome from "./barredora/Home";
+import HojaMantenimientoBarredora from "./barredora/Form";
 
 export default function MantenimientoRoutes() {
   return (
     <Routes>
+      {/* HOME GENERAL */}
       <Route index element={<IndexMantenimiento />} />
-      <Route path="hidro" element={<HojaMantenimientoHidro />} />
-      <Route path="barredora" element={<HojaMantenimientoBarredora />} />
+
+      {/* =============================
+          HIDRO
+      ============================== */}
+      <Route path="hidro">
+        <Route index element={<MantenimientoHidroHome />} />
+        <Route path="crear" element={<HojaMantenimientoHidro />} />
+        <Route path=":id" element={<HojaMantenimientoHidro />} />
+      </Route>
+
+      {/* =============================
+          BARREDORA
+      ============================== */}
+      <Route path="barredora">
+        <Route index element={<MantenimientoBarredoraHome />} />
+        <Route path="crear" element={<HojaMantenimientoBarredora />} />
+        <Route path=":id" element={<HojaMantenimientoBarredora />} />
+      </Route>
+
+      {/* FALLBACK */}
+      <Route path="*" element={<Navigate to="/mantenimiento" replace />} />
     </Routes>
   );
 }
