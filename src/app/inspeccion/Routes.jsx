db8ad305d25b70d9b41c341502f51e7532f32172
Djjasline <134792819+Djjasline@users.noreply.github.com>
2026-01-13 10:17:30 -0500

    Update Routes.jsx

--- src/app/inspeccion/Routes.jsx
+++ src/app/inspeccion/Routes.jsx
@@ -6,23 +6,19 @@ import HojaInspeccionHidro from "./HojaInspeccionHidro";
 import HojaInspeccionBarredora from "./HojaInspeccionBarredora";
 import HojaInspeccionCamara from "./HojaInspeccionCamara";
 import HistorialInspecciones from "./HistorialInspecciones";
-import HojaFirma from "./HojaFirma"; // 👈 IMPORTANTE
+import HojaFirma from "./HojaFirma";
 
 export default function InspectionRoutes() {
   return (
     <Routes>
-      {/* MENÚ */}
       <Route index element={<IndexInspeccion />} />
 
-      {/* HISTORIAL */}
       <Route path="hidro/historial" element={<HistorialInspecciones />} />
 
-      {/* FORMULARIOS */}
       <Route path="hidro/:id" element={<HojaInspeccionHidro />} />
       <Route path="barredora/:id" element={<HojaInspeccionBarredora />} />
       <Route path="camara/:id" element={<HojaInspeccionCamara />} />
 
-      {/* FIRMA (ESTA ERA LA QUE FALTABA) */}
       <Route path="hidro/:id/firma" element={<HojaFirma />} />
     </Routes>
   );
