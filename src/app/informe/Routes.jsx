20295adb460e10e9cc0d336963d1ff6ecb7c64ba
Djjasline <134792819+Djjasline@users.noreply.github.com>
2026-01-13 21:16:21 -0500

    Update Routes.jsx

--- src/app/informe/Routes.jsx
+++ src/app/informe/Routes.jsx
@@ -1,18 +1,14 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 
+import InformeHome from "./InformeHome";
 import NuevoInforme from "./NuevoInforme";
 
 export default function InformeRoutes() {
   return (
     <Routes>
-      {/* HOME (temporalmente es NuevoInforme) */}
-      <Route index element={<NuevoInforme />} />
-
-      {/* CREAR / EDITAR INFORME */}
+      <Route index element={<InformeHome />} />
       <Route path="nuevo" element={<NuevoInforme />} />
       <Route path=":id" element={<NuevoInforme />} />
-
-      {/* FALLBACK */}
       <Route path="*" element={<Navigate to="/informe" replace />} />
     </Routes>
   );
