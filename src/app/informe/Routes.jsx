75f9430581ae80d22900c9a328a78899525696e9
Djjasline <134792819+Djjasline@users.noreply.github.com>
2026-01-13 20:47:45 -0500

    Update Routes.jsx

--- src/app/informe/Routes.jsx
+++ src/app/informe/Routes.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import IndexInforme from "./IndexInforme";
 import NuevoInforme from "./NuevoInforme";
+import InformePDF from "./InformePDF";
 
 export default function InformeRoutes() {
   return (
     <Routes>
       <Route index element={<IndexInforme />} />
       <Route path="nuevo" element={<NuevoInforme />} />
+      <Route path="pdf" element={<InformePDF />} />
     </Routes>
   );
 }
