0224687e0ff716d2b52c32d2a7538be240fe766d
Djjasline <134792819+Djjasline@users.noreply.github.com>
2026-01-13 22:49:15 -0500

    Update Routes.jsx

--- src/app/informe/Routes.jsx
+++ src/app/informe/Routes.jsx
@@ -1,3 +1,5 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+
 import InformeHome from "./InformeHome";
 import NuevoInforme from "./NuevoInforme";
 import InformePDF from "./InformePDF";
@@ -5,9 +7,17 @@ import InformePDF from "./InformePDF";
 export default function InformeRoutes() {
   return (
     <Routes>
+      {/* HOME */}
       <Route index element={<InformeHome />} />
+
+      {/* NUEVO INFORME */}
       <Route path="nuevo" element={<NuevoInforme />} />
+
+      {/* PDF */}
       <Route path="pdf/:id" element={<InformePDF />} />
+
+      {/* FALLBACK */}
+      <Route path="*" element={<Navigate to="/informe" replace />} />
     </Routes>
   );
 }
