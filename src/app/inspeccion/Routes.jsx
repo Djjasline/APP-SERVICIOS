d02ff8ced2b5041f391986a9bf811dfbcb3424fa
Djjasline <134792819+Djjasline@users.noreply.github.com>
2026-01-20 09:14:37 -0500

    Update Routes.jsx

--- src/app/inspeccion/Routes.jsx
+++ src/app/inspeccion/Routes.jsx
@@ -1,34 +1,26 @@
-import React from "react";
 import { Routes, Route } from "react-router-dom";
 
 import IndexInspeccion from "./pages/IndexInspeccion";
 import HojaInspeccionHidro from "./HojaInspeccionHidro";
 import HojaInspeccionBarredora from "./HojaInspeccionBarredora";
 import HojaInspeccionCamara from "./HojaInspeccionCamara";
-import HistorialInspecciones from "./HistorialInspecciones";
 import HojaFirma from "./HojaFirma";
 
-/* ✅ PDF SOLO HIDRO */
+// PDF HIDRO
 import InspeccionHidroPDF from "./pages/InspeccionHidroPDF";
 
 export default function InspectionRoutes() {
   return (
     <Routes>
-      {/* MENÚ PRINCIPAL */}
       <Route index element={<IndexInspeccion />} />
 
-      {/* HISTORIAL (si lo usas) */}
-      <Route path="hidro/historial" element={<HistorialInspecciones />} />
-
-      {/* FORMULARIOS */}
       <Route path="hidro/:id" element={<HojaInspeccionHidro />} />
       <Route path="barredora/:id" element={<HojaInspeccionBarredora />} />
       <Route path="camara/:id" element={<HojaInspeccionCamara />} />
 
-      {/* FIRMA */}
       <Route path="hidro/:id/firma" element={<HojaFirma />} />
 
-      {/* ✅ PDF HIDRO */}
+      {/* PDF */}
       <Route path="hidro/:id/pdf" element={<InspeccionHidroPDF />} />
     </Routes>
   );
