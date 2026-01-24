bc9e5de0789768c2f1630016f83c05fd850619d5
Djjasline <134792819+Djjasline@users.noreply.github.com>
2026-01-24 18:29:44 -0500

    Update Routes.jsx

--- src/app/inspeccion/Routes.jsx
+++ src/app/inspeccion/Routes.jsx
@@ -1,10 +1,11 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import IndexInspeccion from "./pages/IndexInspeccion";
 import HojaInspeccionHidro from "./HojaInspeccionHidro";
 import HojaInspeccionBarredora from "./HojaInspeccionBarredora";
 import HojaInspeccionCamara from "./HojaInspeccionCamara";
 import HojaFirma from "./HojaFirma";
+import InspeccionHidroPdf from "./pages/pdf/InspeccionHidroPdf";
 
 export default function InspectionRoutes() {
   return (
@@ -12,16 +13,12 @@ export default function InspectionRoutes() {
       <Route index element={<IndexInspeccion />} />
 
       <Route path="hidro/:id" element={<HojaInspeccionHidro />} />
+      <Route path="hidro/:id/pdf" element={<InspeccionHidroPdf />} />
+
       <Route path="barredora/:id" element={<HojaInspeccionBarredora />} />
       <Route path="camara/:id" element={<HojaInspeccionCamara />} />
 
       <Route path="hidro/:id/firma" element={<HojaFirma />} />
-
-      {/* 🔒 CAPTURA DE RUTA PDF INVÁLIDA */}
-      <Route
-        path="hidro/:id/pdf"
-        element={<Navigate to="/inspeccion" replace />}
-      />
     </Routes>
   );
 }
