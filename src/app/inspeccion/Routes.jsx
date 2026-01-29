c7888f055572bb3b6624cb05df1c02b61d24a9ac
Djjasline <134792819+Djjasline@users.noreply.github.com>
2026-01-29 10:15:51 -0500

    Update Routes.jsx

--- src/app/inspeccion/Routes.jsx
+++ src/app/inspeccion/Routes.jsx
@@ -1,24 +1,41 @@
 import { Routes, Route } from "react-router-dom";
 
 import IndexInspeccion from "./pages/IndexInspeccion";
+
 import HojaInspeccionHidro from "./HojaInspeccionHidro";
 import HojaInspeccionBarredora from "./HojaInspeccionBarredora";
 import HojaInspeccionCamara from "./HojaInspeccionCamara";
 import HojaFirma from "./HojaFirma";
+
+/* ===== PDFS ===== */
 import InspeccionHidroPdf from "./pages/pdf/InspeccionHidroPdf";
+import InspeccionBarredoraPdf from "./pages/pdf/InspeccionBarredoraPdf";
+import InspeccionCamaraPdf from "./pages/pdf/InspeccionCamaraPdf";
 
 export default function InspectionRoutes() {
   return (
     <Routes>
+      {/* ================= INDEX ================= */}
       <Route index element={<IndexInspeccion />} />
 
+      {/* ================= HIDRO ================= */}
       <Route path="hidro/:id" element={<HojaInspeccionHidro />} />
       <Route path="hidro/:id/pdf" element={<InspeccionHidroPdf />} />
-
-      <Route path="barredora/:id" element={<HojaInspeccionBarredora />} />
-      <Route path="camara/:id" element={<HojaInspeccionCamara />} />
-
       <Route path="hidro/:id/firma" element={<HojaFirma />} />
+
+      {/* ================= BARREDORA ================= */}
+      <Route path="barredora/:id" element={<HojaInspeccionBarredora />} />
+      <Route
+        path="barredora/:id/pdf"
+        element={<InspeccionBarredoraPdf />}
+      />
+
+      {/* ================= CÁMARA ================= */}
+      <Route path="camara/:id" element={<HojaInspeccionCamara />} />
+      <Route
+        path="camara/:id/pdf"
+        element={<InspeccionCamaraPdf />}
+      />
     </Routes>
   );
 }
