d4f05be76f892aa00cc8ef991e9aa91a3df234a3
Djjasline <134792819+Djjasline@users.noreply.github.com>
2026-02-02 12:23:21 -0500

    Create MantenimientoBarredoraPdf.jsx

---
+++ src/app/mantenimiento/pages/pdf/MantenimientoBarredoraPdf.jsx
@@ -0,0 +1,8 @@
+export default function MantenimientoBarredoraPdf() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>PDF Mantenimiento Barredora</h1>
+      <p>En construcción…</p>
+    </div>
+  );
+}
