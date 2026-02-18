9d2872b4935d017710ddb46eea2a8b3a01fa336c
Djjasline <134792819+Djjasline@users.noreply.github.com>
2026-02-18 13:08:00 -0500

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -2,8 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-
 import { ReportProvider } from "./context/ReportContext";
+
+// 👇 IMPORTANTE PARA PWA
+import { registerSW } from "virtual:pwa-register";
+
+registerSW({
+  onNeedRefresh() {
+    console.log("Nueva versión disponible.");
+  },
+  onOfflineReady() {
+    console.log("App lista para trabajar offline.");
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
