6a4d214dcf252fbadd6ecaa9440561def68fd6e3
Djjasline <134792819+Djjasline@users.noreply.github.com>
2026-02-24 10:45:13 -0500

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://asujqyqcjpsqgwrgyrix.supabase.co";
+const supabaseUrl = "https://asujqyqcjpsgqwrgyrjx.supabase.co";
 const supabaseAnonKey = "sb_publishable_3huhP6xU4Xk27W2r-u8WluQ_UJm034jJ";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
