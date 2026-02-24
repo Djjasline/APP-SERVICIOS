bf9e3807cced8baf2969a6fe486f034cee6741f2
Djjasline <134792819+Djjasline@users.noreply.github.com>
2026-02-24 16:14:07 -0500

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://asujqyqcjpsgqwrgyrjx.supabase.co";
-const supabaseAnonKey = "sb_publishable_3huhP6xU4Xk27W2r-u8WluQ_UJm034jJ";
+const supabaseAnonKey = "sb_publishable_3huhP6xU4Xk27W2-u8WluQ_UJm034jJ";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
