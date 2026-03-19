02b769d4ea7c5811070d086bc58823118dfe3634
Djjasline <134792819+Djjasline@users.noreply.github.com>
2026-03-19 14:02:03 -0500

    Update InformeHome.jsx

--- src/app/informe/InformeHome.jsx
+++ src/app/informe/InformeHome.jsx
@@ -1,6 +1,6 @@
 import SyncStatus from "@/components/SyncStatus";
 import { syncReports } from "@/lib/sync";
-import { supabase } from "@/lib/supabase";
+import { supabase } from "../../lib/supabaseClient";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
