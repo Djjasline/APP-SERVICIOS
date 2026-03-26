6c925cbb620dc0ba22f97c1f71ba824a58be3f1b
Djjasline <134792819+Djjasline@users.noreply.github.com>
2026-03-26 11:43:01 -0500

    Update Routes.jsx

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,27 +1,32 @@
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
 
-// PANEL
+// ================= PANEL =================
 import PanelServicios from "./pages/PanelServicios";
 
-// SUBMENÚS (ÁREAS)
+// ================= ÁREAS =================
 import AreaVehiculos from "./pages/AreaVehiculos";
 import AreaAgua from "./pages/AreaAgua";
 import AreaPetroleo from "./pages/AreaPetroleo";
 import AreaOperaciones from "./pages/AreaOperaciones";
 import AreaRepositorios from "./pages/AreaRepositorios";
 
-// INFORMES
+// ================= INFORMES =================
 import InformeHome from "./app/informe/InformeHome";
 import NuevoInforme from "./app/informe/NuevoInforme";
 
-// MANTENIMIENTO
+// ================= INSPECCIONES =================
+import HistorialInspecciones from "./app/inspeccion/HistorialInspecciones";
+import HojaInspeccionHidro from "./app/inspeccion/HojaInspeccionHidro";
+import HojaInspeccionBarredora from "./app/inspeccion/HojaInspeccionBarredora";
+import HojaInspeccionCamara from "./app/inspeccion/HojaInspeccionCamara";
+
+// ================= MANTENIMIENTO =================
 import IndexMantenimiento from "./app/mantenimiento/IndexMantenimiento";
 import HojaMantenimientoHidro from "./app/mantenimiento/HojaMantenimientoHidro";
 import HojaMantenimientoBarredora from "./app/mantenimiento/HojaMantenimientoBarredora";
 
-// OPERACIONES (FORMULARIOS YA EXISTENTES)
+// ================= OPERACIONES =================
 import LiberacionHome from "./app/liberacion/LiberacionHome";
 import LiberacionForm from "./app/liberacion/LiberacionForm";
 
@@ -51,33 +56,32 @@ export default function RoutesApp() {
           <Route path="/informe/:id" element={<NuevoInforme />} />
 
           {/* ================= INSPECCIÓN ================= */}
-<Route path="/inspeccion" element={<IndexInspeccion />} />
+          <Route path="/inspeccion" element={<HistorialInspecciones />} />
 
-{/* HIDRO */}
-<Route path="/inspeccion/hidro/new" element={<HojaInspeccionHidro />} />
-<Route path="/inspeccion/hidro/:id" element={<HojaInspeccionHidro />} />
+          {/* HIDRO */}
+          <Route path="/inspeccion/hidro/new" element={<HojaInspeccionHidro />} />
+          <Route path="/inspeccion/hidro/:id" element={<HojaInspeccionHidro />} />
 
-{/* BARREDORA */}
-<Route path="/inspeccion/barredora/new" element={<HojaInspeccionBarredora />} />
-<Route path="/inspeccion/barredora/:id" element={<HojaInspeccionBarredora />} />
+          {/* BARREDORA */}
+          <Route path="/inspeccion/barredora/new" element={<HojaInspeccionBarredora />} />
+          <Route path="/inspeccion/barredora/:id" element={<HojaInspeccionBarredora />} />
 
-{/* CAMARA */}
-<Route path="/inspeccion/camara/new" element={<HojaInspeccionCamara />} />
-<Route path="/inspeccion/camara/:id" element={<HojaInspeccionCamara />} />
+          {/* CAMARA */}
+          <Route path="/inspeccion/camara/new" element={<HojaInspeccionCamara />} />
+          <Route path="/inspeccion/camara/:id" element={<HojaInspeccionCamara />} />
 
-         {/* ================= MANTENIMIENTO ================= */}
+          {/* ================= MANTENIMIENTO ================= */}
+          <Route path="/mantenimiento" element={<IndexMantenimiento />} />
 
-<Route path="/mantenimiento" element={<IndexMantenimiento />} />
+          {/* HIDRO */}
+          <Route path="/mantenimiento/hidro/new" element={<HojaMantenimientoHidro />} />
+          <Route path="/mantenimiento/hidro/:id" element={<HojaMantenimientoHidro />} />
 
-{/* HIDRO */}
-<Route path="/mantenimiento/hidro/new" element={<HojaMantenimientoHidro />} />
-<Route path="/mantenimiento/hidro/:id" element={<HojaMantenimientoHidro />} />
+          {/* BARREDORA */}
+          <Route path="/mantenimiento/barredora/new" element={<HojaMantenimientoBarredora />} />
+          <Route path="/mantenimiento/barredora/:id" element={<HojaMantenimientoBarredora />} />
 
-{/* BARREDORA */}
-<Route path="/mantenimiento/barredora/new" element={<HojaMantenimientoBarredora />} />
-<Route path="/mantenimiento/barredora/:id" element={<HojaMantenimientoBarredora />} />
-
-          {/* ================= OPERACIONES (FUNCIONALES) ================= */}
+          {/* ================= OPERACIONES ================= */}
           <Route path="/liberacion" element={<LiberacionHome />} />
           <Route path="/liberacion/new" element={<LiberacionForm />} />
 
