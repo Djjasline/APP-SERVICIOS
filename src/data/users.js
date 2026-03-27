88fb931e692bb93fac70369bd6dd3badd7f394cb
Djjasline <134792819+Djjasline@users.noreply.github.com>
2026-03-27 12:19:27 -0500

    Update users.js

--- src/data/users.js
+++ src/data/users.js
@@ -5,7 +5,7 @@ export const USERS = [
   { email: "enieto@astap.com", password: "1717702318*", role: "admin" },
 
   // TECNICOS
-  { email: "abriones@astap.com", password: "Jordan.72*", role: "tecnico" },
+  { email: "abriones@astap.com", password: "Jordan.720", role: "tecnico" },
   { email: "ppaucar@astap.com", password: "171719121*", role: "tecnico" },
   { email: "kleon@astap.com", password: "172340310*", role: "tecnico" },
   { email: "jpillajo@astap.com", password: "171894484*", role: "tecnico" },
