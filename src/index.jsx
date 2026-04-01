bf686ccf52a089a55bfd3038960fa9d3dca1a37e
Djjasline <134792819+Djjasline@users.noreply.github.com>
2026-04-01 15:57:59 -0500

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/index.css";
-import "./pwaUpdater";
+
 // 🔥 CONTEXTOS
 import { AuthProvider } from "./context/AuthContext";
 import { ReportProvider } from "./context/ReportContext";
