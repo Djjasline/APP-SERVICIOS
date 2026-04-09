86a985e28988b848947097bfe40494d7f19402ae
Djjasline <134792819+Djjasline@users.noreply.github.com>
2026-04-09 13:31:02 -0500

    Update NuevoInforme.jsx

--- src/app/vehiculos/informe/NuevoInforme.jsx
+++ src/app/vehiculos/informe/NuevoInforme.jsx
@@ -25,6 +25,7 @@ export default function NuevoInforme() {
   =========================== */
   const emptyReport = {
     referenciaContrato: "",
+    pedidoDemanda: "",
     descripcion: "",
     codInf: "",
 
