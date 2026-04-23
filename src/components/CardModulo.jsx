c48447e137f63a542e58b1935cd46f723576e458
Djjasline <134792819+Djjasline@users.noreply.github.com>
2026-04-23 10:54:07 -0500

    Update CardModulo.jsx

--- src/components/CardModulo.jsx
+++ src/components/CardModulo.jsx
@@ -9,18 +9,21 @@ export default function CardModulo({
   const navigate = useNavigate();
 
   return (
-    <div className="bg-white p-4 rounded shadow">
-      <h2 className="font-semibold text-gray-900">
-        {titulo}
-      </h2>
+    <div className="bg-white p-4 rounded shadow flex flex-col justify-between hover:shadow-lg hover:-translate-y-1 transition duration-300">
 
-      <p className="text-sm text-gray-500">
-        {descripcion}
-      </p>
+      <div>
+        <h2 className="font-semibold text-gray-900">
+          {titulo}
+        </h2>
+
+        <p className="text-sm text-gray-500 mt-1">
+          {descripcion}
+        </p>
+      </div>
 
       <button
         onClick={() => navigate(ruta)}
-        className={`mt-2 ${color} text-white px-3 py-2 rounded`}
+        className={`mt-4 ${color} text-white px-4 py-3 text-sm rounded`}
       >
         Ir
       </button>
