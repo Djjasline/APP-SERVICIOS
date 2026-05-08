9700b150a800abc8fa35cc0f3b9ffa0ef16c3241
Djjasline <134792819+Djjasline@users.noreply.github.com>
2026-05-08 07:37:13 -0500

    Create manualesTecnicos.js

---
+++ src/data/manualesTecnicos.js
@@ -0,0 +1,44 @@
+// src/data/manualesTecnicos.js
+
+export const MANUALES_TECNICOS = [
+  {
+    nombre: "BARREDORAS ELGIN",
+    color: "red",
+    url: "https://astap1-my.sharepoint.com/:f:/g/personal/smaviles_astap_com/IgAAyAcYNCcpQL7on3UsHNy0ARquKZmhsRRAbIN96kw5OTY?e=XrXpa3",
+  },
+  {
+    nombre: "BARREDORAS PIQUERSA",
+    color: "green",
+    url: "https://astap1-my.sharepoint.com/:f:/g/personal/smaviles_astap_com/IgA75mCTM8TrQYpUK477HEk_AcjBzKIVAWuVXbBJdKw66JY?e=jtU0gQ",
+  },
+  {
+    nombre: "BOQUILLAS ENZ",
+    color: "blue",
+    url: "https://astap1-my.sharepoint.com/:f:/g/personal/smaviles_astap_com/IgCheMuDhRARSJHB_wg8Lk8eATBDbwe6sbIrCQt0BPI-Zoo?e=F9cyPB",
+  },
+  {
+    nombre: "CÁMARAS",
+    color: "teal",
+    url: "https://astap1-my.sharepoint.com/:f:/g/personal/smaviles_astap_com/IgBKjDsF03AeR5xWzo8fJE4oAYjpArahflTOvzHdW9sInb4?e=7rBEBJ",
+  },
+  {
+    nombre: "HIDROSUCCIONADORES OTRAS MARCAS",
+    color: "cyan",
+    url: "https://astap1-my.sharepoint.com/:f:/g/personal/smaviles_astap_com/IgByOW3a74wHSI2Izzw3s5_FASclfboLkHG-rOP2WDOPAAo?e=ZBswar",
+  },
+  {
+    nombre: "HIDROSUCCIONADORES VACTOR",
+    color: "blue",
+    url: "https://astap1-my.sharepoint.com/:f:/g/personal/smaviles_astap_com/IgBYQIz5t7T1SZ9jmg_74MNcAR9VLu0VblMLUl9rONBNz-4?e=sAcAPk",
+  },
+  {
+    nombre: "INFORMACIÓN TÉCNICA ADICIONAL VARIADA",
+    color: "sky",
+    url: "https://astap1-my.sharepoint.com/:f:/g/personal/smaviles_astap_com/IgDDlnQ8Hyg2Q5h2efuZoQB_AUhcgLj4YPJRH_PGgFcxpkk?e=qrRJgl",
+  },
+  {
+    nombre: "INFORMACIÓN TÉCNICA VARIADA",
+    color: "yellow",
+    url: "https://astap1-my.sharepoint.com/:f:/g/personal/smaviles_astap_com/IgClFTWFXidKQKTFXn4FTyFTAU9MEj18cw_e_6oDCZBu6pY?e=c98pU6",
+  },
+];
