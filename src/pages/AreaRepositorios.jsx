3fb49b02303e4e45cd8a2fd25cac1c93511606ef
Djjasline <134792819+Djjasline@users.noreply.github.com>
2026-05-08 07:55:48 -0500

    Update AreaRepositorios.jsx

--- src/pages/AreaRepositorios.jsx
+++ src/pages/AreaRepositorios.jsx
@@ -1,26 +1,25 @@
 import CardModulo from "@/components/CardModulo";
-import { FileText, File, Folder } from "lucide-react";
+import { FileText, Database } from "lucide-react";
 
 export default function AreaRepositorios() {
   return (
     <div className="p-6 space-y-6">
-
       {/* HEADER */}
       <div>
         <h2 className="text-lg font-semibold text-white">
           Repositorios
         </h2>
+
         <p className="text-sm text-gray-300">
           Gestión y almacenamiento de documentos y archivos técnicos.
         </p>
       </div>
 
       <div className="grid md:grid-cols-2 gap-6">
-
         <CardModulo
           titulo="Manuales técnicos de vehículos especiales"
-          descripcion="Acceso a manuales técnicos, catalogos y documentación técnica por marca de equipo"
-          ruta="/repositorios/Manuales-tecnicos-de-vehiculos-especiales"
+          descripcion="Acceso a manuales técnicos, catálogos y documentación técnica por marca de equipo."
+          ruta="/repositorios/manuales-tecnicos"
           color="bg-blue-600"
           icono={<FileText size={20} />}
         />
@@ -28,12 +27,11 @@ export default function AreaRepositorios() {
         <CardModulo
           titulo="Base de datos"
           descripcion="Acceso a base de datos empresarial."
-          ruta="/repositorios/Base-de-datos"
+          ruta="/repositorios/base-datos"
           color="bg-red-600"
-          icono={<File size={20} />}
+          icono={<Database size={20} />}
         />
-
-             </div>
+      </div>
     </div>
   );
 }
