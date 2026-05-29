8e7a1cbafdbe56c1062f8e452d874ad123879aff
Djjasline <134792819+Djjasline@users.noreply.github.com>
2026-05-29 15:15:49 -0500

    Update Routes.jsx

--- src/app/operaciones/recepcion/Routes.jsx
+++ src/app/operaciones/recepcion/Routes.jsx
@@ -1,23 +1,16 @@
-import { Routes, Route } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
-import RecepcionHome from "./RecepcionHome";
 import HojaRecepcion from "./HojaRecepcion";
 import HojaRecepcionPDF from "./HojaRecepcionPDF";
+import RecepcionHome from "./RecepcionHome";
 
 export default function RecepcionRoutes() {
   return (
     <Routes>
-
-      {/* HOME */}
       <Route index element={<RecepcionHome />} />
-
-      {/* FORMULARIO */}
       <Route path="new" element={<HojaRecepcion />} />
       <Route path=":id" element={<HojaRecepcion />} />
-
-      {/* PDF */}
       <Route path=":id/pdf" element={<HojaRecepcionPDF />} />
-
     </Routes>
   );
 }
