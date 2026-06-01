63a0ebba22d076ea751e1f5cbf5610a2204768d7
Djjasline <134792819+Djjasline@users.noreply.github.com>
2026-06-01 14:32:46 -0500

    Update Routes.jsx

--- src/app/operaciones/liberacion/Routes.jsx
+++ src/app/operaciones/liberacion/Routes.jsx
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import LiberacionHome from "./LiberacionHome";
 import LiberacionForm from "./LiberacionForm";
 import LiberacionDetalle from "./LiberacionDetalle";
+import LiberacionPDF from "./LiberacionPDF";
 
 export default function LiberacionRoutes() {
   return (
     <Routes>
       <Route index element={<LiberacionHome />} />
       <Route path="nuevo" element={<LiberacionForm />} />
+      <Route path="pdf/:id" element={<LiberacionPDF />} />
       <Route path=":id" element={<LiberacionDetalle />} />
     </Routes>
   );
